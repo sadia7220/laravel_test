86b51366c4eb48444038f514ba7a4e0936a94ee8
sadia7220 <sadia@kludioasia.com>
2022-02-25 21:13:12 +0600

    Multiple database connection

--- database/migrations/2022_02_24_173410_create_activities_table.php
+++ database/migrations/2022_02_24_173410_create_activities_table.php
@@ -13,7 +13,7 @@ class CreateActivitiesTable extends Migration
      */
     public function up()
     {
-        Schema::create('activities', function (Blueprint $table) {
+        Schema::connection('mysql2')->create('activities', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
             $table->string('causer');
